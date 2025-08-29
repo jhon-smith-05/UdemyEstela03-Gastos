a9395a9aa03039a268aea8b0c97f4f8b0f0749a8
Jhon Bernal <jsmithbo05@gmail.com>
2025-08-29 00:34:02 -0500

    sección 27 - 152 Página Login - Diseño

--- src/app/paginas/login/login.component.ts
+++ src/app/paginas/login/login.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TokenService } from '../../service/token.service';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent{
+  
+  usuario:any;
+  constructor(
+    private service: TokenService,
+    //private cookieService: CookieService
+  ){}
+
+  inciarFomulario()
+  {
+    this.usuario={
+      correo:"",
+      password:""
+    };
+  }
+
+  enviar()
+  {
+
+  }
 
 }
