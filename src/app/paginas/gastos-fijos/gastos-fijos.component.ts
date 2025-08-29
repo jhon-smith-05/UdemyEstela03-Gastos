e9106200264208b8168e8192f29ebe633978a5f6
Jhon Bernal <jsmithbo05@gmail.com>
2025-08-28 23:10:15 -0500

    sección 26 - 144 Creacion de mas componentes login, gastos fijos, etc

---
+++ src/app/paginas/gastos-fijos/gastos-fijos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gastos-fijos',
+  standalone: true,
+  imports: [],
+  templateUrl: './gastos-fijos.component.html',
+  styleUrl: './gastos-fijos.component.css'
+})
+export class GastosFijosComponent {
+
+}
---
+++ src/app/paginas/gastos-por-dia/gastos-por-dia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gastos-por-dia',
+  standalone: true,
+  imports: [],
+  templateUrl: './gastos-por-dia.component.html',
+  styleUrl: './gastos-por-dia.component.css'
+})
+export class GastosPorDiaComponent {
+
+}
