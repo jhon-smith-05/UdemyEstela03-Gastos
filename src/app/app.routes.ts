87eeaf71e1e36f4a615c1fa8843d388a96876d70
Jhon Bernal <jsmithbo05@gmail.com>
2025-08-29 01:22:37 -0500

    sección 27 - 159 implementacion de guard en las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,15 +6,16 @@ import { UsuariosComponent } from './paginas/usuarios/usuarios.component';
 import { ProveedoresComponent } from './paginas/proveedores/proveedores.component';
 import { ErrorComponent } from './paginas/error/error.component';
 import { LoginComponent } from './paginas/login/login.component';
+import { AuthGuard } from './guard/auth.guards';
 
 export const routes: Routes = 
 [
-    {path:"", component: HomeComponent},
+    {path:"", component: HomeComponent, canActivate: [AuthGuard]},
+    {path:"usuarios", component: UsuariosComponent, canActivate: [AuthGuard]},
+    {path:"proveedores", component: ProveedoresComponent, canActivate: [AuthGuard]},
+    {path:"gastos-por-dia", component: GastosPorDiaComponent, canActivate: [AuthGuard]},
+    {path:"gastos-fijos", component: GastosFijosComponent, canActivate: [AuthGuard]},
     {path:"login", component: LoginComponent},
-    {path:"usuarios", component: UsuariosComponent},
-    {path:"proveedores", component: ProveedoresComponent},
-    {path:"gastos-por-dia", component: GastosPorDiaComponent},
-    {path:"gastos-fijos", component: GastosFijosComponent},
 
     {path:"**", component: ErrorComponent},
 
