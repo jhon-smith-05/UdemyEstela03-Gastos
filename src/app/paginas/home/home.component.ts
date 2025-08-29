ff3673df4dacbb61f1aeb932c4ba49d0c7ef63de
Jhon Bernal <jsmithbo05@gmail.com>
2025-08-28 21:57:11 -0500

    sección 26 - 139 Creación de componentes de menu, header y footer

--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from "../../componentes/menu/menu.component";
+import { HeaderComponent } from "../../componentes/header/header.component";
+import { FooterComponent } from "../../componentes/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styles: ``
 })
