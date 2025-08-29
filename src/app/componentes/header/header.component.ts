14d7f0637f777a84a6a2977d54a3a96b5a61dde4
Jhon Bernal <jsmithbo05@gmail.com>
2025-08-28 22:52:04 -0500

    sección 26 - 142 Creacion de reloj digital en componente header

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import 'dayjs/locale/es';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  
+  hora:any;
+  fecha:any;
+  ngOnInit(): void {
+    this.getHoraActual();
+    this.fecha = Date();
+   
+  }
+
+  getHoraActual()
+  {
+    this.hora = new Date();
+    setInterval(() => {
+      this.hora = new Date();
+      }, 1000
+    );
+  }
 
 }
