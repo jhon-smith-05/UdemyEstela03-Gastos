2169f1a53bdf70f13dd4d66fb1ed181831463a9a
Jhon Bernal <jsmithbo05@gmail.com>
2025-09-05 00:36:21 -0500

    sección 29 - 173 Servicio proveedores state (signalSlice)

--- src/app/service/proveedores-state.service.ts
+++ src/app/service/proveedores-state.service.ts
@@ -1,9 +1,53 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Proveedores } from '../interfaces/proveedores';
+import { ProveedoresRequest } from '../interfaces/proveedores-request';
+import { ProveedoresService } from './proveedores.service';
+import { signalSlice } from 'ngxtension/signal-slice';
+import { catchError, map, of } from 'rxjs';
+
+
+interface State {
+  datos: Proveedores[];
+  status: 'loading' | 'success' | 'error';
+}
+
+interface StateSend {
+  datos: ProveedoresRequest | null;
+  loaded: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProveedoresStateService {
 
-  constructor() { }
+private proveedoresService = inject(ProveedoresService);
+
+//listar
+private initialState: State = 
+{
+  datos:[],
+  status: 'loading' as const
+};
+
+state = signalSlice({
+  initialState: this.initialState,
+  sources: [
+    this.proveedoresService.getProveedores().pipe(
+      map((datos) => ({datos, status: 'success' as const})),
+      catchError(() => 
+        {
+          return of({
+            datos: [],
+            status: 'error' as const
+          })
+        })
+    ),
+  ]
+});
+
+//crear
+
+
+//editar
 }
