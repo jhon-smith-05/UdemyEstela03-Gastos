71403fb881d3f618c7bdd14ec91a988d1e1b4c58
Jhon Bernal <jsmithbo05@gmail.com>
2025-09-05 00:11:39 -0500

    sección 29 - 170 diseño de proveedores usando signalSlice

--- src/app/paginas/proveedores/proveedores.component.ts
+++ src/app/paginas/proveedores/proveedores.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../componentes/footer/footer.component";
+import { HeaderComponent } from "../../componentes/header/header.component";
+import { MenuComponent } from "../../componentes/menu/menu.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-proveedores',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, HeaderComponent, MenuComponent, RouterLink],
   templateUrl: './proveedores.component.html',
   styleUrl: './proveedores.component.css'
 })
 export class ProveedoresComponent {
+
+  datos:any;
+
+
+  crear()
+  {
+
+  }
+
+  enviar()
+  {
+
+  }
+
+  editar(data: any)
+  {
+
+  }
+
+  eliminar(id: any)
+  {
+
+  }
 
 }
