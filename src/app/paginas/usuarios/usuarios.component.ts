c51b3b723371db415e5acb1e5539d7377ffcb04f
Jhon Bernal <jsmithbo05@gmail.com>
2025-09-04 20:14:04 -0500

    sección 28 - 163 diseño de pagina para usuarios

--- src/app/paginas/usuarios/usuarios.component.ts
+++ src/app/paginas/usuarios/usuarios.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from "../../componentes/menu/menu.component";
+import { HeaderComponent } from "../../componentes/header/header.component";
+import { FooterComponent } from "../../componentes/footer/footer.component";
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css'
 })
 export class UsuariosComponent {
+
+  datos:any;
+
+  crear()
+  {
+
+  }
+
+  editar(dato:any)
+  {
+
+  }
 
 }
