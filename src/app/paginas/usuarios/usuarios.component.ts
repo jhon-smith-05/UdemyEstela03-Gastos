8455b2eb7039d214959c27065876452416f6d309
Jhon Bernal <jsmithbo05@gmail.com>
2025-09-04 21:21:17 -0500

    sección 28 - 165 listar usuarios

--- src/app/paginas/usuarios/usuarios.component.ts
+++ src/app/paginas/usuarios/usuarios.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuComponent } from "../../componentes/menu/menu.component";
 import { HeaderComponent } from "../../componentes/header/header.component";
 import { FooterComponent } from "../../componentes/footer/footer.component";
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { UsuariosService } from '../../service/usuarios.service';
 
 @Component({
   selector: 'app-usuarios',
@@ -11,9 +12,32 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css'
 })
-export class UsuariosComponent {
+export class UsuariosComponent implements OnInit {
 
   datos:any;
+  constructor(
+    private servicio: UsuariosService
+  ){}
+
+  ngOnInit(): void {
+    this.hacerPeticion();
+  }
+
+  hacerPeticion()
+  {
+    this.servicio.getUsuarios().subscribe({
+      next: data => 
+        {
+          this.datos = data;
+          console.log(this.datos);
+      },
+      error: error => 
+        {
+          console.log('Error', error.message);
+      }
+    });
+
+  }
 
   crear()
   {
