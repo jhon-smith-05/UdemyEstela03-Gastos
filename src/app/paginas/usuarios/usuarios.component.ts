0b05a74addac8b1fa0e7599020845f022494b40e
Jhon Bernal <jsmithbo05@gmail.com>
2025-09-04 21:45:10 -0500

    sección 28 - 166 implementacion ventana modal con Bootstrap

--- src/app/paginas/usuarios/usuarios.component.ts
+++ src/app/paginas/usuarios/usuarios.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, viewChild, ViewChild } from '@angular/core';
 import { MenuComponent } from "../../componentes/menu/menu.component";
 import { HeaderComponent } from "../../componentes/header/header.component";
 import { FooterComponent } from "../../componentes/footer/footer.component";
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { UsuariosService } from '../../service/usuarios.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
 
 @Component({
   selector: 'app-usuarios',
@@ -15,8 +16,13 @@ import { UsuariosService } from '../../service/usuarios.service';
 export class UsuariosComponent implements OnInit {
 
   datos:any;
+  modalTitle!: string;
+
+   @ViewChild("myModalConf", {static: false}) myModalConf!: TemplateRef<any>;
+
   constructor(
-    private servicio: UsuariosService
+    private servicio: UsuariosService,
+    private modalService: NgbModal
   ){}
 
   ngOnInit(): void {
@@ -41,6 +47,18 @@ export class UsuariosComponent implements OnInit {
 
   crear()
   {
+    this.modalService.open(this.myModalConf, {size: 'lg'});
+    this.modalTitle = 'Crear';
+  }
+
+  cerrar()
+  {
+    this.modalService.dismissAll();
+
+  }
+
+  enviar()
+  {
 
   }
 
@@ -48,5 +66,7 @@ export class UsuariosComponent implements OnInit {
   {
 
   }
+
+  
 
 }
