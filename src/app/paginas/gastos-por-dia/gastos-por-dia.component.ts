4240974f7182242b44865b2576f8495bc8908ba9
Jhon Bernal <jsmithbo05@gmail.com>
2025-09-06 00:43:30 -0500

    sección 31 - 189 Diseño de página de gastos por día

--- src/app/paginas/gastos-por-dia/gastos-por-dia.component.ts
+++ src/app/paginas/gastos-por-dia/gastos-por-dia.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuComponent } from "../../componentes/menu/menu.component";
+import { HeaderComponent } from "../../componentes/header/header.component";
+import { FooterComponent } from "../../componentes/footer/footer.component";
+import { RouterLink } from '@angular/router';
+import dayjs from 'dayjs';
+import { DatePipe } from '@angular/common';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-gastos-por-dia',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent, HeaderComponent, FooterComponent, RouterLink, DatePipe],
   templateUrl: './gastos-por-dia.component.html',
   styleUrl: './gastos-por-dia.component.css'
 })
-export class GastosPorDiaComponent {
+export class GastosPorDiaComponent implements OnInit{
+
+  fecha: any;
+
+  constructor(
+    private modalService: NgbModal
+  ){}
+
+  ngOnInit(): void {
+    this.fecha = new Date();
+    this.getMesActual();
+  }
+ 
+  getMesActual()
+  {
+    let date=new Date();
+    dayjs.locale('es') ;
+    return dayjs(date).format("MMMM");
+  }
+
+  cerrar()
+  {
+    this.modalService.dismissAll();
+  }
+
+  crear()
+  {
+
+  }
 
 }
