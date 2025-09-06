6839b1354468762f6f5050dd18f8dd069cdef99b
Jhon Bernal <jsmithbo05@gmail.com>
2025-09-05 21:55:35 -0500

    sección 30 - 178 Cargar año y fecha actual

--- src/app/paginas/gastos-fijos/gastos-fijos.component.ts
+++ src/app/paginas/gastos-fijos/gastos-fijos.component.ts
@@ -1,22 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuComponent } from "../../componentes/menu/menu.component";
 import { HeaderComponent } from "../../componentes/header/header.component";
 import { FooterComponent } from "../../componentes/footer/footer.component";
 import { RouterLink } from '@angular/router';
+import dayjs from 'dayjs';
+import "dayjs/locale/es";
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-gastos-fijos',
   standalone: true,
-  imports: [MenuComponent, HeaderComponent, FooterComponent, RouterLink],
+  imports: [MenuComponent, HeaderComponent, FooterComponent, RouterLink, DatePipe],
   templateUrl: './gastos-fijos.component.html',
   styleUrl: './gastos-fijos.component.css'
 })
-export class GastosFijosComponent {
+export class GastosFijosComponent implements OnInit {
+  
+  fecha:any;
 
+  ngOnInit(): void {
+    this.fecha = new Date();
+  }
 
   crear()
   {
 
+  }
+
+  getMesActual()
+  {
+    let date = new Date();
+    dayjs.locale('es');
+    return dayjs(date).format("MMMM");
   }
 
 
